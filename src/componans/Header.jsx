17869779cff2ee870d745761a5c5c5e0f092c1a8
Michael Essam <maichelessam2001@gmail.com>
2026-03-18 21:14:43 +0200

    fix page three

--- src/componans/Header.jsx
+++ src/componans/Header.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header>
-            <div className="left">
-                <img className="logo" src="/logo.png" alt="logo" />
-                <img className="eid-elfeter" src="/eid-elfeter.png" alt="" />
-            </div>
-            <div className="right">
-                <img src="/google.png" alt="" />
-            </div>
+            <Link to={'/'}>
+                <div className="left">
+                    <img className="logo" src="/logo.png" alt="logo" />
+                    <img className="eid-elfeter" src="/eid-elfeter.png" alt="" />
+                </div>
+                <div className="right">
+                    <img src="/google.png" alt="" />
+                </div>
+            </Link>
         </header>
     );
 };
